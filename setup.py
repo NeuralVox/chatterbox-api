58689872e5f9c2601205967903ad07b0bc10526b
mrfakename <me@mrfake.name>
2025-06-03 20:45:18 -0700

    Bump to 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="chatterbox-server",
     description="A API for ChatterboxTTS",
-    version="0.1.3",
+    version="0.1.4",
     author="mrfakename",
     author_email="me@mrfake.name",
     url="https://github.com/neuralvox/chatterbox-api",
